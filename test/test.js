10d1d9a0b18ace7b186a87419e1532844e93f760
Pavel Kolesnikov <pavel@gooddata.com>
2010-07-12 22:14:48 +0200

    log messages on stderr

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var csv  = require('../lib/ya-csv'),
+    sys  = require('sys');
+
+sys.debug('start');
+
+if (process.argv.length < 3) {
+    sys.error("Usage: node " + process.argv[1] + " <csv file>");
+    process.exit(1);
+}
+
+var file = process.argv[2];
+
+var csvIn = csv.createCsvFileReader(file, {
+    'separator': ':',
+    'comment': '#',
+});
+var csvOut = new csv.CsvWriter(process.stdout);
+
+csvIn.addListener('end', function() {
+    sys.debug('end');
+});
+csvIn.addListener('data', function(data) {
+    csvOut.writeRecord(data);
+});
