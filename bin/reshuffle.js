b078225ccdfb9fabcf82e636b46af421e8d2a48d
Pavel Kolesnikov <pavel@gooddata.com>
2010-07-25 17:20:25 +0200

    added reshuffle.js - a simple script to strip and/or reorder columns of a CSV stream on stdin

---
+++ bin/reshuffle.js
@@ -0,0 +1,25 @@
+var csv = require('../lib/ya-csv'),
+    sys = require('sys');
+
+var reader = csv.createCsvStreamReader(process.openStdin(), { columnsFromHeader: true });
+var writer = csv.createCsvStreamWriter(process.stdout);
+
+if (process.argv.length < 3) {
+    sys.error("Usage: " + process.argv[0] + " " + process.argv[1] + " <output columns>");
+    process.exit(1);
+}
+
+var outColumns = [];
+for (var i = 2; i < process.argv.length; i++) {
+    outColumns.push(process.argv[i]);
+}
+
+writer.writeRecord(outColumns);
+reader.addListener('data', function(data) {
+    var out = [];
+    for (var i = 0; i < outColumns.length; i++) {
+        col = outColumns[i];
+        out[i] = data[col];
+    }
+    writer.writeRecord(out);
+});
